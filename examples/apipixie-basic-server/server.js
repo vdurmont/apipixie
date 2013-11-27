f3be039f3acc1d314ffe51205aa56947a11d48b7
Vincent DURMONT <vdurmont@gmail.com>
2013-11-27 20:42:22 +0100

    Deletion of obsolete console.log

--- examples/apipixie-basic-server/server.js
+++ examples/apipixie-basic-server/server.js
@@ -16,21 +16,17 @@ var messages = [
 app.get('/messages', function(req, res) {
 	console.log("Received a request to get all the messages.");
 	res.json(messages);
-	end();
 });
 
 app.get('/messages/:id', function(req, res) {
 	var id = req.params.id;
 	console.log("Received a request to get the message#"+id+".");
 	res.json(messages[id-1]);
-	end();
 });
 
 app.put('/messages/:id', function(req, res) {
 	var id = req.params.id;
 	var input = req.body;
-
-	console.dir(input)
 
 	console.log("Received a request to update the message#"+id);
 
@@ -38,17 +34,8 @@ app.put('/messages/:id', function(req, res) {
 	input.id = parseInt(id);
 
 	messages[id-1] = input;
-
-	console.log("return:");
-	console.dir(messages[id-1]);
 	res.json(messages[id-1]);
-	end();
 });
-
-var end = function(){
-	console.log("MESSAGES:");
-	console.dir(messages);
-}
 
 app.listen(1337);
 console.log("Express server listening on port 1337");
